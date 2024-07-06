dfa5b7f76d65313eb324f4e529872a94f23ad221
Sandee <102047643+Sandee004@users.noreply.github.com>
2024-07-06 15:01:07 +0000

    Render hosting

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -1,6 +1,5 @@
 import Navbar from './components/navbar';
 import Homepage from './components/homepage';
-import Services from './components/services';
 import Portfolio from './components/portfolio';
 import Collab from './components/collab';
 
--- landing-page/src/components/collab.tsx
+++ landing-page/src/components/collab.tsx
@@ -1,4 +1,4 @@
-import img1 from '../images/img1.png';
+
 const Collab = () => {
     return (
         <>
@@ -12,8 +12,8 @@ const Collab = () => {
             </div>
         </div>
 
-            <footer class="bg-black text-white py-4 px-5 flex flex-wrap justify-around">
-              <div class="w-full sm:w-1/3 mb-4 md:mb-0">
+            <footer className="bg-black text-white py-4 px-5 flex flex-wrap justify-around">
+              <div className="w-full sm:w-1/3 mb-4 md:mb-0">
                   <div className="flex my-5">
                       <svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path fill-rule="evenodd" clip-rule="evenodd" d="M20.4376 7.43769L14.2658 0.932373L8.0941 7.43769L0 11.2971L4.09935 18.8449L22.1355 10.9997L4.95855 24.3381L5.44907 28.0676L14.2658 26.4324L23.0826 28.0676L24.2519 19.1771L28.5317 11.2971L20.4376 7.43769Z" fill="white"/>
@@ -21,13 +21,13 @@ const Collab = () => {
                       <p className="mx-1 text-xl">Creativo.</p>
                   </div>
                 <p>Creativo is where your imagination and our expertise converge to create design solutions that elevate your brand and set you apart from the competition.</p>
-              <p class="font-bold text-[20px] mt-5">hello@creativo.com</p>
-                <p class="font-bold text-[20px]">+1-800-123 4567</p>
+              <p className="font-bold text-[20px] mt-5">hello@creativo.com</p>
+                <p className="font-bold text-[20px]">+1-800-123 4567</p>
               </div>
-              <div class="grid grid-cols-2 sm:grid-cols-3 gap-20 row-gap-2 sm:w-1/3 text-sm mt-5">
+              <div className="grid grid-cols-2 sm:grid-cols-3 gap-20 row-gap-2 sm:w-1/3 text-sm mt-5">
                 <div>
-                  <h3 class="font-bold mb-2">Company</h3>
-                  <ul class="list-none pl-0">
+                  <h3 className="font-bold mb-2">Company</h3>
+                  <ul className="list-none pl-0">
                     <li className="py-1">
                       <a href="#" className="text-gray-300 hover:text-white py-2">About</a>
                     </li>
@@ -38,7 +38,7 @@ const Collab = () => {
                       <a href="#" className="text-gray-300 hover:text-white">Careers</a>
                     </li>
                     <li className="py-1">
-                      <a href="#" class="text-gray-300 hover:text-white">Blog</a>
+                      <a href="#" className="text-gray-300 hover:text-white">Blog</a>
                     </li>
                       <li className="py-1">
                       <a href="#" className="text-gray-300 hover:text-white">Pricing</a>
@@ -46,8 +46,8 @@ const Collab = () => {
                   </ul>
                 </div>
                   <div>
-                    <h3 class="font-bold mb-2">Contact</h3>
-                    <ul class="list-none pl-0">
+                    <h3 className="font-bold mb-2">Contact</h3>
+                    <ul className="list-none pl-0">
                       <li className="py-1">
                         <a href="#" className="text-gray-300 hover:text-white py-2">Help</a>
                       </li>
@@ -58,7 +58,7 @@ const Collab = () => {
                         <a href="#" className="text-gray-300 hover:text-white">Press</a>
                       </li>
                       <li className="py-1">
-                        <a href="#" class="text-gray-300 hover:text-white">Terms & Conditions</a>
+                        <a href="#" className="text-gray-300 hover:text-white">Terms & Conditions</a>
                       </li>
                         <li className="py-1">
                         <a href="#" className="text-gray-300 hover:text-white">Partners</a>
@@ -66,8 +66,8 @@ const Collab = () => {
                     </ul>
                   </div>
                   <div>
-                    <h3 class="font-bold mb-2">Discover</h3>
-                    <ul class="list-none pl-0">
+                    <h3 className="font-bold mb-2">Discover</h3>
+                    <ul className="list-none pl-0">
                       <li className="py-1">
                         <a href="#" className="text-gray-300 hover:text-white py-2">Affiliate</a>
                       </li>
@@ -78,7 +78,7 @@ const Collab = () => {
                         <a href="#" className="text-gray-300 hover:text-white">Partner Program</a>
                       </li>
                       <li className="py-1">
-                        <a href="#" class="text-gray-300 hover:text-white">Blog</a>
+                        <a href="#" className="text-gray-300 hover:text-white">Blog</a>
                       </li>
                         <li className="py-1">
                         <a href="#" className="text-gray-300 hover:text-white">Pricing</a>
